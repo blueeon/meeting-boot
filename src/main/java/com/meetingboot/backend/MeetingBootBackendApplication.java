6b725bc178bc84d08953d4a163b7d7ed327f7438
yangke <blueeon@blueeon.net>
2018-11-29 10:47:10 +0800

    init spring boot

---
+++ src/main/java/com/meetingboot/backend/MeetingBootBackendApplication.java
@@ -0,0 +1,12 @@
+package com.meetingboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingBootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingBootBackendApplication.class, args);
+    }
+}
